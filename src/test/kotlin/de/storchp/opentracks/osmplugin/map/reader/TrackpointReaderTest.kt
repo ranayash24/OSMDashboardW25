a0f9952076d7e7df8f947d05c8087fc7d9e1bbd1
WaseemJan063 <waseemjan6659@gmail.com>
2025-03-15 17:59:24 -0400

    removal of duplicated code

--- src/test/kotlin/de/storchp/opentracks/osmplugin/map/reader/TrackpointReaderTest.kt
+++ src/test/kotlin/de/storchp/opentracks/osmplugin/map/reader/TrackpointReaderTest.kt
@@ -22,11 +22,16 @@ class TrackpointReaderTest {
     private lateinit var contentResolver: ContentResolver
     private lateinit var cursor: Cursor
     private val testUri: Uri = mockk()
+    private lateinit var trackpoint1: Trackpoint
+    private lateinit var trackpoint2: Trackpoint
 
     @BeforeEach
     fun setUp() {
         contentResolver = mockk()
         cursor = mockk()
+
+        trackpoint1 = createTrackpoint()
+        trackpoint2 = createTrackpoint().copy(id = 3L, type = TRACKPOINT_TYPE_PAUSE, speed = 0.0)
 
         every { cursor.getColumnIndexOrThrow(TrackpointReader.ID) } returns 1
         every { cursor.getColumnIndexOrThrow(TrackpointReader.TRACKID) } returns 2
@@ -37,12 +42,30 @@ class TrackpointReaderTest {
         every { cursor.close() } just Runs
     }
 
+    /**
+     * Helper function to validate trackpoints and avoid duplication.
+     */
+    private fun validateTrackpoints(
+        trackpoints: List<List<Trackpoint>>,
+        expectedDebug: TrackpointsDebug,
+        segments: Int
+    ) {
+        assertThat(trackpoints).hasSize(1)
+        assertThat(trackpoints[0]).hasSize(2)
+        assertThat(trackpoints[0][0]).isEqualTo(trackpoint1)
+        assertThat(trackpoints[0][1]).isEqualTo(trackpoint2)
+        assertThat(trackpoints.debug).isEqualTo(
+            TrackpointsDebug(
+                trackpointsReceived = expectedDebug.trackpointsReceived,
+                trackpointsInvalid = expectedDebug.trackpointsInvalid,
+                trackpointsPause = expectedDebug.trackpointsPause,
+                segments = segments,
+            )
+        )
+    }
+
     @Test
     fun readTracksV1() {
-        var trackpoint1 = createTrackpoint()
-        var trackpoint2 =
-            createTrackpoint().copy(id = 3L, type = TRACKPOINT_TYPE_PAUSE, speed = 0.0)
-
         every {
             contentResolver.query(
                 testUri,
@@ -52,56 +75,22 @@ class TrackpointReaderTest {
                 null
             )
         } returns cursor
+
         every { cursor.getColumnIndex(TrackpointReader.TYPE) } returns -1
         every { cursor.moveToNext() } returnsMany listOf(true, true, true, false)
         every { cursor.getLong(1) } returnsMany listOf(trackpoint1.id!!, trackpoint2.id!!, 0)
-        every { cursor.getLong(2) } returnsMany listOf(
-            trackpoint1.trackId!!,
-            trackpoint2.trackId!!,
-            0
-        )
-        every { cursor.getInt(3) } returnsMany listOf(
-            trackpoint1.latLong.latitudeE6,
-            PAUSE_LAT_LONG,
-            0
-        )
-        every { cursor.getInt(4) } returnsMany listOf(
-            trackpoint1.latLong.longitudeE6,
-            PAUSE_LAT_LONG,
-            0
-        )
-        every { cursor.getLong(5) } returnsMany listOf(
-            trackpoint1.time!!.toEpochMilli(),
-            trackpoint2.time!!.toEpochMilli(),
-            0
-        )
-        every { cursor.getDouble(6) } returnsMany listOf(
-            trackpoint1.speed!!,
-            trackpoint2.speed!!,
-            0.0
-        )
+        every { cursor.getLong(2) } returnsMany listOf(trackpoint1.trackId!!, trackpoint2.trackId!!, 0)
+        every { cursor.getInt(3) } returnsMany listOf(trackpoint1.latLong.latitudeE6, PAUSE_LAT_LONG, 0)
+        every { cursor.getInt(4) } returnsMany listOf(trackpoint1.latLong.longitudeE6, PAUSE_LAT_LONG, 0)
+        every { cursor.getLong(5) } returnsMany listOf(trackpoint1.time!!.toEpochMilli(), trackpoint2.time!!.toEpochMilli(), 0)
+        every { cursor.getDouble(6) } returnsMany listOf(trackpoint1.speed!!, trackpoint2.speed!!, 0.0)
 
         val trackpoints = TrackpointReader.readTrackpointsBySegments(contentResolver, testUri, 1, 1)
-
-        assertThat(trackpoints).hasSize(1)
-        assertThat(trackpoints[0]).hasSize(2)
-        assertThat(trackpoints[0][0]).isEqualTo(trackpoint1)
-        assertThat(trackpoints[0][1]).isEqualTo(trackpoint2)
-        assertThat(trackpoints.debug).isEqualTo(
-            TrackpointsDebug(
-                trackpointsReceived = 3,
-                trackpointsInvalid = 1,
-                trackpointsPause = 1,
-                segments = 1,
-            )
-        )
+        validateTrackpoints(trackpoints, TrackpointsDebug(3, 1, 1, 1), 1)
     }
 
     @Test
     fun readTracksV2() {
-        var trackpoint1 = createTrackpoint()
-        var trackpoint2 =
-            createTrackpoint().copy(id = 3L, type = TRACKPOINT_TYPE_PAUSE, speed = 0.0)
         every {
             contentResolver.query(
                 testUri,
@@ -111,56 +100,24 @@ class TrackpointReaderTest {
                 null
             )
         } returns cursor
+
         every { cursor.getColumnIndex(TrackpointReader.TYPE) } returns 7
         every { cursor.moveToNext() } returnsMany listOf(true, true, true, false)
         every { cursor.getLong(1) } returnsMany listOf(trackpoint1.id!!, trackpoint2.id!!, 0)
-        every { cursor.getLong(2) } returnsMany listOf(
-            trackpoint1.trackId!!,
-            trackpoint2.trackId!!,
-            0
-        )
-        every { cursor.getInt(3) } returnsMany listOf(
-            trackpoint1.latLong.latitudeE6,
-            PAUSE_LAT_LONG,
-            (130 * APIConstants.LAT_LON_FACTOR).toInt(),
-        )
-        every { cursor.getInt(4) } returnsMany listOf(
-            trackpoint1.latLong.longitudeE6,
-            PAUSE_LAT_LONG,
-            0,
-        )
-        every { cursor.getLong(5) } returnsMany listOf(
-            trackpoint1.time!!.toEpochMilli(),
-            trackpoint2.time!!.toEpochMilli(),
-            0,
-        )
-        every { cursor.getDouble(6) } returnsMany listOf(
-            trackpoint1.speed!!,
-            trackpoint2.speed!!,
-            0.0,
-        )
-        every { cursor.getInt(7) } returnsMany listOf(
-            trackpoint1.type,
-            trackpoint2.type,
-            TRACKPOINT_TYPE_TRACKPOINT,
-        )
+        every { cursor.getLong(2) } returnsMany listOf(trackpoint1.trackId!!, trackpoint2.trackId!!, 0)
+        every { cursor.getInt(3) } returnsMany listOf(trackpoint1.latLong.latitudeE6, PAUSE_LAT_LONG, (130 * APIConstants.LAT_LON_FACTOR).toInt())
+        every { cursor.getInt(4) } returnsMany listOf(trackpoint1.latLong.longitudeE6, PAUSE_LAT_LONG, 0)
+        every { cursor.getLong(5) } returnsMany listOf(trackpoint1.time!!.toEpochMilli(), trackpoint2.time!!.toEpochMilli(), 0)
+        every { cursor.getDouble(6) } returnsMany listOf(trackpoint1.speed!!, trackpoint2.speed!!, 0.0)
+        every { cursor.getInt(7) } returnsMany listOf(trackpoint1.type, trackpoint2.type, TRACKPOINT_TYPE_TRACKPOINT)
 
         val trackpoints = TrackpointReader.readTrackpointsBySegments(contentResolver, testUri, 1, 2)
-
-        assertThat(trackpoints).hasSize(1)
-        assertThat(trackpoints[0]).hasSize(2)
-        assertThat(trackpoints[0][0]).isEqualTo(trackpoint1)
-        assertThat(trackpoints[0][1]).isEqualTo(trackpoint2)
-        assertThat(trackpoints.debug).isEqualTo(
-            TrackpointsDebug(
-                trackpointsReceived = 3,
-                trackpointsInvalid = 1,
-                trackpointsPause = 1,
-                segments = 1,
-            )
-        )
+        validateTrackpoints(trackpoints, TrackpointsDebug(3, 1, 1, 1), 1)
     }
 
+    /**
+     * Creates a mock Trackpoint for testing.
+     */
     private fun createTrackpoint() = Trackpoint(
         id = 2L,
         trackId = 1L,
